70547b6b1fba7493aaae4e86d7a0ab2ef8e129fb
Jonathan Stegall <jstegall@minnpost.com>
2016-04-11 14:21:36 -0500

    php code we have to add to the end of wp-config and run only once, then remove.
    it tries to fix permalinks

---
+++ addtoconfig.php
@@ -0,0 +1,39 @@
+// We prepare a variable to hold arrays with post titles so we don't accidentally make duplicates, that would be bad
+$slug_done = array();
+// Run a query to grab all the posts, we only want posts/pages though
+$posts = $wpdb->get_results( "
+SELECT
+`ID`,
+`post_title`
+FROM
+`" . $wpdb->posts . "`
+WHERE
+`post_type` = 'page'
+OR
+`post_type` = 'post'
+" );
+// Loop through results
+foreach( $posts AS $single )
+{
+// Generate a URL friendly slug from the title
+$slug_base = sanitize_title_with_dashes( $single->post_title );
+$this_slug = $slug_base;
+$slug_num = 1;
+// Check if the slug already exists, if it does, we add an incremental integer ot the end of it
+while (in_array( $this_slug, $slug_done ) )
+{
+$this_slug = $slug_base . '-' . $slug_num;
+$slug_num++;
+}
+$slug_done[] = $this_slug;
+// We are happy with our slug, update the database table
+$wpdb->query( "
+UPDATE
+`" . $wpdb->posts . "`
+SET
+`post_name` = '" . $this_slug . "'
+WHERE
+`ID` = '" . $single->ID . "'
+LIMIT 1
+" );
+}
