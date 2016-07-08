000b3c0703c000742e59c3651f618e083afdc6da
Jonathan Stegall <jstegall@minnpost.com>
2016-07-08 15:51:04 -0500

    add some new queries to check the matching on story/author pairs. still a bit nervous

--- results.sql
+++ results.sql
@@ -72,9 +72,11 @@ SELECT
 
 
 # Count how many users we added as terms and term_taxonomy
-# 2001 users for both queries on 6/29/16; this is correct
-SELECT COUNT(*) FROM wp_terms WHERE term_group=1
-SELECT count(*) FROM wp_term_taxonomy WHERE taxonomy='author'
+# 2001 users for these queries on 6/29/16; this is correct
+SELECT COUNT(*) FROM wp_terms WHERE term_group=1;
+# also could use this
+SELECT count(*) FROM wp_term_taxonomy WHERE taxonomy='author';
+
 
 
 # get count of author/story pairs that we think we're adding compared to what is already in drupal
@@ -120,8 +122,52 @@ SELECT
 ;
 
 
+
+# this should show us the count of authors per story that are in drupal but not in wordpress
+# 7/8/16 zero results
+SELECT count(*), au.nid
+FROM `minnpost.092515`.content_field_op_author au
+WHERE au.field_op_author_nid IS NOT NULL AND NOT EXISTS
+(
+	SELECT count(*), r.object_id
+	FROM `minnpost.wordpress`.wp_term_relationships r
+	WHERE r.object_id = au.nid
+	GROUP BY object_id
+)
+GROUP BY au.nid
+;
+
+
+
+
+
+# this should show us the count of authors per story that are in wordpress but not in drupal
+# 7/8/16 zero results
+SELECT COUNT(*), r.object_id
+FROM `minnpost.wordpress`.wp_term_relationships r
+INNER JOIN wp_term_taxonomy tax ON r.term_taxonomy_id = tax.term_taxonomy_id
+WHERE tax.taxonomy = 'author' AND NOT EXISTS
+(
+	SELECT COUNT(*), au.nid
+	FROM `minnpost.092515`.content_field_op_author au
+	WHERE au.field_op_author_nid IS NOT NULL AND au.nid = r.object_id
+	GROUP BY au.nid
+)
+GROUP BY r.object_id
+;
+
+
+
+
+
 # Get post IDs where the author does not match
 # as of 5/19/16 there are 333 of these or 2793 if we do not use distinct
+# as of 6/30/16 there are 810 of these or 8045 if we do not use distinct. So apparenty getting worse.
+# as of 7/1/16 it was 331, and 2789, when trying a different query to create the matches. sigh.
+# 7/5/16 - tried going into the WP interface, on one particular article. compared the authors to drupal. there was only one in wp, two in drupal, so added one. this reduced the count in this query as would be expected.
+# 7/5/16 reordering the authors in the ui appears to delete both rows
+# 7/5/16 has 810 rows, though. no difference between ascending and descending order, apparently
+# 7/6/16 - if I use this query instead, joining on the value of the term, no rows are returned. This is as it should be?
 
 SELECT DISTINCT `minnpost.wordpress`.p.ID as wordpress_post_id, `minnpost.wordpress`.t.name as wordpress_author_name, n.nid as drupal_id, au.title as drupal_author_name
 FROM `minnpost.wordpress`.wp_posts p
@@ -130,6 +176,7 @@ INNER JOIN `minnpost.wordpress`.wp_term_taxonomy tax ON tax.term_taxonomy_id = r
 INNER JOIN `minnpost.wordpress`.wp_terms t ON t.term_id = tax.term_id
 LEFT OUTER JOIN `minnpost.092515`.node n ON p.ID = n.nid
 LEFT OUTER JOIN `minnpost.092515`.content_field_op_author a ON n.nid = a.nid
-LEFT OUTER JOIN `minnpost.092515`.node au ON a.field_op_author_nid = au.nid
+LEFT OUTER JOIN `minnpost.092515`.node au ON t.name = au.title
 WHERE tax.taxonomy = 'author'
 AND `minnpost.wordpress`.t.name != au.title
+;
